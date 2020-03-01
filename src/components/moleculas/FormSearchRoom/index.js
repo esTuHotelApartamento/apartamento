82e23d97a2611026dee27936527d052cc35afef4
esTuHotelApartamento <estuhotelapt@gmail.com>
2020-03-01 09:27:09 +0100

    layout

--- src/components/moleculas/FormSearchRoom/index.js
+++ src/components/moleculas/FormSearchRoom/index.js
@@ -1,18 +1,16 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react"
 
-import 'react-dates/initialize';
-import 'react-dates/lib/css/_datepicker.css';
-import './dataRangerPicker.css'
-import { DateRangePicker } from 'react-dates';
-import esLocale from 'moment/locale/es'
-import moment from 'moment'
-import {navigate} from 'gatsby'
+import "react-dates/initialize"
+import "react-dates/lib/css/_datepicker.css"
+import "./dataRangerPicker.css"
+import { DateRangePicker } from "react-dates"
+import esLocale from "moment/locale/es"
+import moment from "moment"
+import { navigate } from "gatsby"
 
+import { useForm } from "react-hook-form"
 
-import { useForm } from 'react-hook-form'
-
-
-import styled from 'styled-components'
+import styled from "styled-components"
 
 const FormContainer = styled.form`
   padding: 24px 26px;
@@ -24,8 +22,8 @@ const FormContainer = styled.form`
   border-radius: 12px;
   max-width: 500px;
   background-color: white;
-  box-shadow: 0 16px 40px rgba(0,0,0,0.12);
-  
+  box-shadow: 0 16px 40px rgba(0, 0, 0, 0.12);
+
   @media (max-width: 700px) {
     margin-top: 42px;
     max-width: 100%;
@@ -33,16 +31,14 @@ const FormContainer = styled.form`
     box-shadow: none;
     padding: 24px 0px;
   }
-
 `
 const ItemForm = styled.label`
-  margin: ${({ margin }) => margin ? margin : "0px"};
+  margin: ${({ margin }) => (margin ? margin : "0px")};
   width: 100%;
   display: flex;
   align-items: center;
-  justify-content: ${({ justifyContent }) => justifyContent ? justifyContent : "space-between"};
-
-
+  justify-content: ${({ justifyContent }) =>
+    justifyContent ? justifyContent : "space-between"};
 `
 
 const ButtomFormSubmit = styled.input`
@@ -55,10 +51,10 @@ const ButtomFormSubmit = styled.input`
   background-color: ${({ theme }) => theme.colors.emphasis};
   font-size: 18px;
   width: 52%;
-  outline:none;
+  outline: none;
   cursor: pointer;
   @media (max-width: 400px) {
-   width: 100%; 
+    width: 100%;
   }
 `
 
@@ -66,14 +62,13 @@ const ErrorMsj = styled.div`
   color: red;
   font-size: 10px;
 `
-// Input suma y resta de huespedes 
+// Input suma y resta de huespedes
 const InputContContainer = styled.div`
   width: fit-content;
   padding: 12px;
   display: flex;
   justify-content: space-around;
   align-items: center;
-
 `
 const BtnCont = styled.div`
   display: inline-block;
@@ -86,11 +81,11 @@ const BtnCont = styled.div`
   cursor: pointer;
   color: white;
   background-color: ${({ theme }) => theme.colors.secondary.main};
-  transition: 300ms background-color; 
-  margin: 0px 6px; 
+  transition: 300ms background-color;
+  margin: 0px 6px;
   user-select: none;
-  &:hover{
-    background-color:  rgba(24, 44, 53, 1);
+  &:hover {
+    background-color: rgba(24, 44, 53, 1);
   }
 `
 const InputsHuespedContainer = styled.div`
@@ -103,7 +98,7 @@ const InputsHuespedContainer = styled.div`
   @media (max-width: 670px) {
     label {
       max-width: 100%;
-    } 
+    }
   }
 `
 const FraseBienvenida = styled.p`
@@ -111,63 +106,66 @@ const FraseBienvenida = styled.p`
   font-weight: bold;
   color: ${({ theme }) => theme.colors.secondary.main};
 
-  @media (max-width: 478px){
+  @media (max-width: 478px) {
     font-size: 28px;
   }
   @media (max-width: 700px) {
-   color: ghostwhite;
-
+    color: ghostwhite;
   }
 `
 
-
-const FormSearchRoom = (props) => {
-  const { minHeight } = props;
+const FormSearchRoom = props => {
+  const { minHeight } = props
 
   const { register, handleSubmit, setValue, errors } = useForm()
-  const onSubmit = data => { console.log(data) }
-
-  const [initialDate, SetInitialDate] = useState(null);
-  const [finalDate, SetFinalDate] = useState(null);
-  const [focusInputDates, SetFocusInputDates] = useState(null);
-
-  const [CantAdult, setCantAdult] = useState(1);
-  const [CantChildrens, setCantChildrens] = useState(0);
-
-  const [isMobile, setIsMobile] = useState((typeof window !== `undefined`)?(window.innerWidth <= 700) ? true : false : false)
-
-  const handleChangeDates = ({ startDate, endDate }) => {
-    setValue("initialDateInput", startDate);
-    setValue("finalDateInput", endDate);
-    SetInitialDate(startDate);
-    SetFinalDate(endDate);
+  const onSubmit = data => {
+    console.log(data)
   }
 
+  const [initialDate, SetInitialDate] = useState(null)
+  const [finalDate, SetFinalDate] = useState(null)
+  const [focusInputDates, SetFocusInputDates] = useState(null)
+
+  const [CantAdult, setCantAdult] = useState(1)
+  const [CantChildrens, setCantChildrens] = useState(0)
+
+  const [isMobile, setIsMobile] = useState(
+    typeof window !== `undefined`
+      ? window.innerWidth <= 700
+        ? true
+        : false
+      : false
+  )
+
+  const handleChangeDates = ({ startDate, endDate }) => {
+    setValue("initialDateInput", startDate)
+    setValue("finalDateInput", endDate)
+    SetInitialDate(startDate)
+    SetFinalDate(endDate)
+  }
 
   const handleCalc = (field, operation) => {
     if (operation === "add") {
       if (field === "CantAdult") {
-        if( CantAdult + 1 > 8) return
-        setValue("CantAdult", CantAdult + 1);
+        if (CantAdult + 1 > 8) return
+        setValue("CantAdult", CantAdult + 1)
         setCantAdult(CantAdult + 1)
-      }
-      else {
-        if( CantChildrens + 1 > 5) return
-        setValue("CantChildrens", CantChildrens + 1);
+      } else {
+        if (CantChildrens + 1 > 5) return
+        setValue("CantChildrens", CantChildrens + 1)
         setCantChildrens(CantChildrens + 1)
       }
     } else {
       if (field === "CantAdult") {
-        if( CantAdult - 1 < 1) return
-        setValue("CantAdult", CantAdult - 1);
+        if (CantAdult - 1 < 1) return
+        setValue("CantAdult", CantAdult - 1)
         setCantAdult(CantAdult - 1)
       } else {
-        if( CantChildrens - 1  < 0) return
-        setValue("CantChildrens", CantChildrens - 1);
+        if (CantChildrens - 1 < 0) return
+        setValue("CantChildrens", CantChildrens - 1)
         setCantChildrens(CantChildrens - 1)
       }
     }
-
   }
 
   const handleEventResize = () => {
@@ -179,18 +177,18 @@ const FormSearchRoom = (props) => {
     }
   }
   const handleResize = () => {
-    window.addEventListener('resize', handleEventResize)
+    window.addEventListener("resize", handleEventResize)
   }
   useEffect(() => {
-    register({ name: "initialDateInput" });
-    register({ name: "finalDateInput" });
-    register({ name: "CantAdult" });
-    register({ name: "CantChildrens" });
+    register({ name: "initialDateInput" })
+    register({ name: "finalDateInput" })
+    register({ name: "CantAdult" })
+    register({ name: "CantChildrens" })
     handleResize()
   }, [register])
 
   // Config lenguaje datePickers
-  moment.locale("es", esLocale);
+  moment.locale("es", esLocale)
   return (
     <FormContainer minHeight={minHeight} onSubmit={handleSubmit(onSubmit)}>
       <FraseBienvenida>
@@ -208,36 +206,42 @@ const FormSearchRoom = (props) => {
           onDatesChange={handleChangeDates} // PropTypes.func.isRequired,
           focusedInput={focusInputDates} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
           onFocusChange={focusedInput => SetFocusInputDates(focusedInput)} // PropTypes.func.isRequired,
-          orientation={isMobile? "vertical" : "horizontal"}
+          orientation={isMobile ? "vertical" : "horizontal"}
         />
       </ItemForm>
-
-
       Huéspedes
       <InputsHuespedContainer>
         <ItemForm>
           Adultos
-        <InputContContainer>
-            <BtnCont onClick={() => handleCalc('CantAdult', 'subtract ')}>-</BtnCont>
+          <InputContContainer>
+            <BtnCont onClick={() => handleCalc("CantAdult", "subtract ")}>
+              -
+            </BtnCont>
             {CantAdult}
-            <BtnCont onClick={() => handleCalc('CantAdult', 'add')}>+</BtnCont>
+            <BtnCont onClick={() => handleCalc("CantAdult", "add")}>+</BtnCont>
           </InputContContainer>
         </ItemForm>
         <ItemForm>
           Niños
-        <InputContContainer>
-            <BtnCont onClick={() => handleCalc('CantChildrens', 'subtract')}>-</BtnCont>
+          <InputContContainer>
+            <BtnCont onClick={() => handleCalc("CantChildrens", "subtract")}>
+              -
+            </BtnCont>
             {CantChildrens}
-            <BtnCont onClick={() => handleCalc('CantChildrens', 'add')}>+</BtnCont>
+            <BtnCont onClick={() => handleCalc("CantChildrens", "add")}>
+              +
+            </BtnCont>
           </InputContContainer>
         </ItemForm>
       </InputsHuespedContainer>
-
       <ItemForm justifyContent="flex-end" margin="36px 0px 0px">
-        <ButtomFormSubmit type="submit" value="Buscar" onClick={()=> navigate("/reservar/")} />
+        <ButtomFormSubmit
+          type="submit"
+          value="Buscar"
+          onClick={() => navigate("/reservar/")}
+        />
       </ItemForm>
     </FormContainer>
-
   )
 }
 
--- wrap-component.js
+++ wrap-component.js
@@ -4,16 +4,19 @@ import { Provider } from "react-redux"
 import createStore from "./redux/store"
 import theme from "./src/config/theming"
 import { ThemeProvider } from "styled-components"
+import Layout from "./src/layouts"
 
 // eslint-disable-next-line react/display-name,react/prop-types
-export default ({ element }) => {
+export default ({ element, ...props }) => {
   // Instantiating store in `wrapRootElement` handler ensures:
   //  - there is fresh store for each SSR page
   //  - it will be called only once in browser, when React mounts
   const store = createStore()
   return (
     <Provider store={store}>
-      <ThemeProvider theme={theme}>{element}</ThemeProvider>
+      <Layout {...props}>
+        <ThemeProvider theme={theme}>{element}</ThemeProvider>
+      </Layout>
     </Provider>
   )
 }
